b2a9ec1e118ff10f16d7eaf424e77e476ee44d89
patrickroelofs <contact@patrickroelofs.com>
2025-10-05 16:17:53 +0200

    create initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251005_141741 from './20251005_141741';
+
+export const migrations = [
+  {
+    up: migration_20251005_141741.up,
+    down: migration_20251005_141741.down,
+    name: '20251005_141741'
+  },
+];
